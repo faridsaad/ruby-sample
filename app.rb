ab4b08bd4924104c5d1ce07fb432897054e13601
Farid Saad <faradio@gmail.com>
2019-05-29 13:42:21 -0500

    Adding timestamp

--- app.rb
+++ app.rb
@@ -5,9 +5,12 @@ require 'bundler/setup'
 require 'sinatra'
 require 'json'
 
+variables = ['HOSTNAME','OPENSHIFT_BUILD_NAME']
+
 get '/' do
-  output = "Hello world!</br>"
-  env_string = JSON.pretty_generate(ENV.to_a).gsub!("\n",'</br>')
-  output += "Environment: </br> #{env_string} </br>"
+  output = "Hello.</br>"
+  #output += ENV.to_a.to_s
+  mylist = ENV.to_a.select {|x| variables.include?(x[0]) }[0]
+  output += JSON.pretty_generate(mylist) + DateTime.now.strftime('</br>%H:%M:%S')
   output
 end
