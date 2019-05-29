31274bdaf031a2a5e4cce0e6ee8db4a16f9b14b0
Farid Saad <faradio@gmail.com>
2019-05-29 15:56:23 -0500

    Small change to output

--- app.rb
+++ app.rb
@@ -1,16 +1,16 @@
 require 'rubygems'
 require 'bundler/setup'
-
-# myapp.rb
 require 'sinatra'
 require 'json'
 
-variables = ['HOSTNAME','OPENSHIFT_BUILD_NAME']
+variables = ['HOSTNAME','OPENSHIFT_BUILD_NAME','OPENSHIFT_BUILD_COMMIT']
 
 get '/' do
-  output = "Hello.</br>"
-  #output += ENV.to_a.to_s
-  mylist = ENV.to_a.select {|x| variables.include?(x[0]) }[0]
-  output += JSON.pretty_generate(mylist) + DateTime.now.strftime('</br>%H:%M:%S')
-  output
+    output =""
+    mylist = ENV.to_a.select {|x| variables.include?(x[0]) }
+    mylist.each do |x|
+        output += "#{x[0]}=#{x[1]}</br>"
+    end
+    output += DateTime.now.strftime('</br>%H:%M:%S')
+    output
 end
